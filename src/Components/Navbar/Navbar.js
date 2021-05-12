a706d2e837d1c83b00ff318ce3142fe1f8157c23
Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>
2021-05-12 18:55:08 +0530

    making changes

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Navbar (){
+    return(
+        <div>
+            <ol>
+                <li>
+                    About us
+                </li>
+                <li>
+                    sign up 
+                </li>
+            </ol>
+        </div>
+    );
+}
