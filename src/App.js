85b77eb59c28d8ed2e87c6f6ee92611c543b4fdb
Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>
2021-05-13 23:33:08 +0530

    Doing it all over again

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import LandingPage from './Components/LandingPage.js/LandingPage';
-import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
-      {/* <Navbar /> */}
     </div>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,15 +2,6 @@ import React from 'react';
 
 export default function Navbar (){
     return(
-        <div>
-            <ol>
-                <li>
-                    About us
-                </li>
-                <li>
-                    sign up 
-                </li>
-            </ol>
-        </div>
+        
     );
 }
